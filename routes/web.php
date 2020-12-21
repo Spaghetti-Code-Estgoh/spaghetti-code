6bbf4d15682d12cc481bbfefa82b89bdc2ea5192
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2020-12-21 17:59:44 +0000

    Register as a bug idk why dummy works fine

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::view('/', 'welcome');
+
+
+//Route::get('/post/{id}/{welcome?}', [PostController::class, 'show']);
+
+Route::get('/post/', [PostController::class, 'index'])->name('post.index')->middleware('auth');
+
+Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+
+Route::post('/post', [PostController::class, 'store'])->name('post.store');
+
+Route::get('/post/{id}/', [PostController::class, 'show'])->name('post.show');
+
+Route::delete('post/{id}', [PostController::class, 'destroy'])->name('post.destroy');
+
+Auth::routes([
+    //'register' => false
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/loginWorker', 'auth.loginWorker');
