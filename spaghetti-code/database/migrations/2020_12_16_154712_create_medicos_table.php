a8622a1fadf10f4e75187978634c5af4a8a2bec2
Diogo Pinto <diogo.martinho.pinto@hotmail.com>
2020-12-23 19:59:11 +0000

    migration & models2

---
+++ spaghetti-code/database/migrations/2020_12_16_154712_create_medicos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->timestamps();
+            $table->string('especialidae');
+            $table->integer('numeroCeluta');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('password');
+            $table->string('fotoPerfil');
+            $table->integer('nif');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
---
+++ spaghetti-code/database/migrations/2020_12_16_162259_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('dataHora')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('descricao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
