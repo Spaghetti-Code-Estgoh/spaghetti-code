d41c0473f6a7c28c003282c3a83f3e5a510d04fb
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-01-01 11:28:04 +0000

    Forgot Password Mail and Blade Views

---
+++ resources/views/emails/reset.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# Pedido para alteração da password
+<br>
+Olá! Foi requesitado um pedido para mudança de password para a tua conta.<br>
+Para realziares a alteração basta carregares no botão onde serás levado para uma página
+para introduzires a tua password nova.<br>
+Caso não tenhas sido tu a fazeres este pedido, ignora o simplesmente que o pedido de alteração
+será eliminado em 48 horas.
+
+@component('mail::button2', ['url' => ''])
+Alterar Password
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
