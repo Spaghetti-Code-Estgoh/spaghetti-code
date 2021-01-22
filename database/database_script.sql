b0a82370bbb2c82d8983a8c328badadc05dcbaa6
unknown <afonsovitorio@hotmail.com>
2021-01-22 11:07:31 +0000

    Adição do script para a criação da base de dados

---
+++ database/database_script.sql
@@ -0,0 +1,196 @@
+/*
+Script para a criação da base de dados
+(não está na versão final)
+Autor: Afonso Vitório 
+*/
+
+
+CREATE DATABASE IF NOT EXISTS `scmed`;
+USE `scmed`;
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contacto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nif` int(11) NOT NULL,
+  `genero` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `morada` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fotoperfil` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `funcionario_email_unique` (`email`),
+  UNIQUE KEY `funcionario_nif_unique` (`nif`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `medicos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `especialidae` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numeroCeluta` int(11) NOT NULL,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fotoPerfil` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nif` int(11) NOT NULL,
+  `remember_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `medicos_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `utentes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nif` int(11) NOT NULL,
+  `nss` int(11) NOT NULL,
+  `genero` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `morada` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dataNascimento` date NOT NULL,
+  `imagePath` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contacto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `tokenConfirm` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `confirmed` int(1) NOT NULL DEFAULT 0,
+  `remember_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `utentes_email_unique` (`email`),
+  UNIQUE KEY `utentes_nif_unique` (`nif`),
+  UNIQUE KEY `utentes_nss_unique` (`nss`),
+  UNIQUE KEY `utentes_tokenConfirm_unique` (`tokenConfirm`)
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+CREATE TABLE IF NOT EXISTS `consulta` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `estado` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `DataHora` timestamp NOT NULL DEFAULT current_timestamp(),
+  `DataHoraFim` timestamp NOT NULL DEFAULT current_timestamp(),
+  `observacoesmedicas` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `observacoesadmin` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `medico_id` bigint(20) unsigned NOT NULL,
+  `funcionario_id` bigint(20) unsigned DEFAULT NULL,
+  `utente_id` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `preco` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Index 2` (`medico_id`),
+  KEY `FK3` (`funcionario_id`),
+  KEY `FK_4` (`utente_id`),
+  CONSTRAINT `FK3` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`),
+  CONSTRAINT `FK_consulta_medicos` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`),
+  CONSTRAINT `FK_utentes_id` FOREIGN KEY (`utente_id`) REFERENCES `utentes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `dataHora` timestamp NOT NULL DEFAULT current_timestamp(),
+  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `notificacoes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `idRecetor` int(11) NOT NULL,
+  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dataHora` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` tinyint(1) NOT NULL,
+  `tipo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `titulo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+insert into utentes
+(nome, email, password, nif, nss, genero, dataNascimento, contacto, confirmed)
+values
+('Test Utente', 'user@teste.com', '$2y$10$lS0CKwJyXZZqf1M/TuXOhOwdFUx68cp/7.tl1bH9YrCmzHPdtDDfK', '159753456', '753159456', 'masculino', '2020-12-31', '159753456', '1');
+
+
+insert into admins
+(nome, email, password)
+values
+('Admin Test', 'admin@teste.com', '$2y$10$lS0CKwJyXZZqf1M/TuXOhOwdFUx68cp/7.tl1bH9YrCmzHPdtDDfK');
+
+
+insert into medicos
+(nome, email, password, especialidae, numeroCeluta, fotoPerfil, nif)
+values
+('Test Medico', 'user@teste.com', '$2y$10$lS0CKwJyXZZqf1M/TuXOhOwdFUx68cp/7.tl1bH9YrCmzHPdtDDfK', 'Medicina Geral', 420, 'default.png', 159456753);
+
+insert into funcionario
+(nome, email, password, contacto, fotoPerfil, genero, morada, nif)
+values
+('Admin Test', 'admin@teste.com', '$2y$10$lS0CKwJyXZZqf1M/TuXOhOwdFUx68cp/7.tl1bH9YrCmzHPdtDDfK', 159456789, 'default.png', 'm', 'casa', 159456789);
+
+
