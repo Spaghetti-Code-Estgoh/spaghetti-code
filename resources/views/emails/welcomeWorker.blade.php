e25d9d9eea0bcd1855ef469741f28b4addc48e8d
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-02-02 10:45:47 +0000

    Create funcionario and medico bug

--- resources/views/emails/welcomeWorker.blade.php
+++ resources/views/emails/welcomeWorker.blade.php
@@ -2,7 +2,7 @@
 # Criação da Conta
 
 Olá! E bem vindo a família SCMed. Esperemos que você tenha muito sucesso na nossa emperesa!
-Após receber este email já irá conseguir fazer login na sua conta utilizando
+Após receber este email já irá conseguir fazer login na sua conta utilizando as credenciais facultadas.
 
 @component('mail::button3', ['url' => '/'])
 Começe a utilizar a plataforma!
